7983de40bf6cd4a0224050ad044a779b6463b23a
itsREV <imtherealrev@gmail.com>
2018-02-28 09:18:43 -0800

    Add files via upload

---
+++ ydl-test.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+def grab(link):
+    path = (input("Where do you want to download it:"))
+    while len(path) == 0:
+        path =(input("Please select a directory:"))
+    else:
+        pass
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': f'{path}\%(title)s.%(ext)s',  #path will be defined on main.py
+    #'simulate' : True,    #only here for testing
+    'default_search' : 'auto',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',     
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+    return		
+
+
+
+url = input("Search for a song or enter a link:")
+
+while len(url) == 0:
+    
+    print ("No url entered")
+    url = input("Please enter a URL")
+    
+else:
+    grab(url)
+    
+    
+        
+    
